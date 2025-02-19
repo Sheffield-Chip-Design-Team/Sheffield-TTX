31484b2b4e1d06f8fa24579c2a60479247adcf51
AbuSonic <babliabdulatif@gmail.com>
2025-02-19 10:57:29 +0000

    Create audiotestconstraints.v

---
+++ src/audiotestconstraints.v
@@ -0,0 +1,6 @@
+set_property IOSTANDARD LVCMOS33 [get_ports clk]
+set_property IOSTANDARD LVCMOS33 [get_ports pwm]
+set_property IOSTANDARD LVCMOS33 [get_ports rst]
+set_property PACKAGE_PIN E3 [get_ports clk]
+set_property PACKAGE_PIN A11 [get_ports pwm]
+set_property PACKAGE_PIN V10 [get_ports rst]
