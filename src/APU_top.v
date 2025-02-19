2c2b2abfecf82d7d42081b65fbc2e16d2253d189
AbuSonic <babliabdulatif@gmail.com>
2025-02-19 10:56:10 +0000

    Update APU_top.v
     renamed variables and removed ASYNC reset

--- src/APU_top.v
+++ src/APU_top.v
@@ -1,27 +1,34 @@
 module PWM(
     input clk,       // System clock
     input rst,       // Reset signal
-    output reg buzzer // PWM output for buzzer
+    output reg pwm // PWM output for buzzer
 );
     
     parameter PWM_PERIOD = 12500; // Define period directly (50 MHz clk and 4kHz pwm)
     parameter NOTE_PERIOD = 250000000; // 5 seconds assuming 50MHz clock (50M * 5)
+   
+    parameter FULL_CYCLE = 128; 
+    parameter HALF_CYCLE = 64; 
+    parameter QUARTER_CYCLE = 32; 
+    parameter EIGTH_CYCLE = 16; 
     
-    reg [7:0] duty_cycle1 = 128/256; // Original duty cycle
-    reg [7:0] duty_cycle2 = 64/256; // Quarter duty cycle
-    reg [7:0] duty_cycle3 = 32/256; // Half duty cycle
-    reg [7:0] duty_cycle4 = 16/256; // Eighth duty cycle
+    
+    reg [7:0] duty_cycle1 = FULL_CYCLE/FULL_CYCLE; // Original duty cycle
+    reg [7:0] duty_cycle2 = HALF_CYCLE/FULL_CYCLE; // Half duty cycle
+    reg [7:0] duty_cycle3 = QUARTER_CYCLE/FULL_CYCLE; // Quater duty cycle
+    reg [7:0] duty_cycle4 = EIGTH_CYCLE/FULL_CYCLE; // Eighth duty cycle
     
     reg [7:0] current_duty_cycle;
     reg [31:0] counter;
     reg [31:0] note_counter;
     reg [1:0] note_index; // 2-bit index to cycle through 4 notes
     
-    always @(posedge clk or posedge rst) begin
+    always @(posedge clk) begin
+       
         if (rst) begin
             counter <= 0;
             note_counter <= 0;
-            buzzer <= 0;
+            pwm <= 0;
             note_index <= 0;
             current_duty_cycle <= duty_cycle1;
 
@@ -32,7 +39,7 @@ module PWM(
             else
                 counter <= 0;
 
-            buzzer <= (counter < (current_duty_cycle * PWM_PERIOD)) ? 1 : 0;
+            pwm <= (counter < (current_duty_cycle * PWM_PERIOD)) ? 1 : 0; 
             
             // Note Alternation Logic
             if (note_counter < NOTE_PERIOD - 1)
