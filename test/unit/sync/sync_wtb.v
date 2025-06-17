99252c3a743e647a0e78ad044c6e2ff96bb4135e
James <162901067+Enjimneering@users.noreply.github.com>
2025-06-17 20:51:35 +0100

    Update sync wrapper

--- test/unit/sync/sync_wtb.v
+++ test/unit/sync/sync_wtb.v
@@ -3,31 +3,37 @@
 
 /* 
   Note:
-    This wrapper just instantiates the module and makes some convenient
-    wires that can be driven / tested by the cocotb python testbench.
+    This wrapper 'testbench' just instantiates the module and makes some
+    convenient wires that can be driven/tested by the cocotb python testbench.
 */
 
-module sync_tb ();
+module sync_tb();
 
   // inputs
   reg clk;
   reg rst_n;
+
   // outputs
-  wire hsync, vsync, video_active, frame_end;
+  wire hsync;
+  wire vsync;
+  wire video_active;
+  wire frame_end;
   wire [9:0] pix_x, pix_y;
+
+  // unused
   // wire enable_input;
 
   sync_generator sync_gen (
-        .clk(clk),
-        .reset(~rst_n),
-        .hsync(hsync),
-        .vsync(vsync),
-        .display_on(video_active),
-        .screen_hpos(pix_x),
-        .screen_vpos(pix_y),
-        .frame_end(frame_end)
-        // .input_enable(enable_input)
-    );
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .screen_hpos(pix_x),
+    .screen_vpos(pix_y),
+    .frame_end(frame_end)
+    // .input_enable(enable_input)
+  );
 
   // Dump the signals to a VCD file so it can be viewed in surfer/GTKWAVE.
   initial begin
